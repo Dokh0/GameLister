ab78acc8444004eaae311bdba6094cf3127a1d48
Dokh0 <palbabyshark@gmail.com>
2023-10-21 17:17:18 +0100

    checking connection in progress

---
+++ api/routes/index.js
@@ -0,0 +1,13 @@
+/* const router = require('express').Router()
+
+
+//importar archivos rutas
+const userRouter = require('./user.router.js')
+
+
+//definir rutas
+router.user('./user', userRouter)
+
+
+//exportar modulo
+module.exports = router */
