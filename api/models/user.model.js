f6bfa74bae22336391165d3eb9e2297ffef1654f
yeray <yeray.programacion.web@gmail.com>
2023-10-22 19:24:25 +0100

    user.models added

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize')
+const { sequelize } = require('../../database')
+const User = sequelize.define('user', {
+        name: { 
+            type: DataTypes.STRING, allowNull: false,
+         },
+        username: {
+             type: DataTypes.STRING, allowNull: false,
+             },
+        email: {
+             type: DataTypes.STRING, allowNull: false,
+             },
+        password: { 
+            type: DataTypes.STRING, allowNull: false,
+         },
+        role: {
+             type: DataTypes.STRING, allowNull: false,
+             }
+    },
+
+    { 
+     timestamps: false 
+}) 
+
+module.exports = User
