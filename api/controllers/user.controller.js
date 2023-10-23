d9351ee8f512c39d75674af075a63bc89b1a4b0f
TheshiaR <cuentaparaclassroomtheshia@gmail.com>
2023-10-23 16:42:53 +0100

    usuarios agregados funciones probadas

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -76,16 +76,6 @@ async function getProfile(req, res) {
 //     }
 // }
 
-
-// async function createUser(req, res) {
-//     try {
-//         const user = await User.create(req.body)
-//         return res.status(200).json({ message: 'User created', user: user })
-//     } catch (error) {
-//         res.status(500).send(error.message)
-//     }
-// }
-
 async function updateUser(req, res) {
     try {
         const [userExist, user] = await User.update(req.body, {
@@ -125,7 +115,6 @@ module.exports = {
     getAllUser,
     getOneUser,
     getProfile,
-    // createUser,
     updateUser,
     deleteUser,
     // getOneUserLazy,
--- api/routes/user.router.js
+++ api/routes/user.router.js
@@ -6,7 +6,6 @@ const { checkAuth, checkAdmin }= require('../middleware/index')
 router.get('/', checkAuth, checkAdmin, getAllUser)
 router.get('/:id', checkAuth, checkAdmin, getOneUser)
 router.get('/:id', checkAuth, getProfile)
-// router.post('/', createUser)
 router.put('/:id', checkAuth, updateUser)
 router.put('/:id', checkAuth, deleteUser)
 
