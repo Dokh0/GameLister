87b0c6271fa17f471cd163bb59eb076be13083b9
TheshiaR <cuentaparaclassroomtheshia@gmail.com>
2023-10-23 16:58:23 +0100

    arreglar platform controller version

--- api/routes/platform.router.js
+++ api/routes/platform.router.js
@@ -1,12 +1,13 @@
 const router = require('express').Router()
 
 const { getAllPlatforms, getOnePlatform, createPlatform, updatePlatform, deletePlatform } = require('../controllers/platform.controller')
+const { checkAuth, checkAdmin }= require('../middleware/index')
 
 router.get('/', getAllPlatforms)
 router.get('/:id', getOnePlatform)
-router.post('/', createPlatform)
-router.put('/:id', updatePlatform)
-router.put('/:id', deletePlatform)
+router.post('/', checkAuth, checkAdmin, createPlatform)
+router.put('/:id', checkAuth, checkAdmin, updatePlatform)
+router.put('/:id', checkAuth, checkAdmin, deletePlatform)
 
 
 module.exports = router
