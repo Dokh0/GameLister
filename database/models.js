b8cfe7182d1086dce720699d3579165647a4ecb6
Dokho <palbabyshark@gmail.com>
2023-10-24 11:09:23 +0100

    relations done

--- database/models.js
+++ database/models.js
@@ -4,12 +4,30 @@ const User = require('../api/models/user.model')
 const Platform = require('../api/models/platform.model')
 const Catalogue = require("../api/models/catalogue.model")
 const Collection = require("../api/models/collection.model")
+const User_catalogue = require('../api/models/user_catalogue.model')
+const Platform_catalogue = require('../api/models/platform_catalogue.model')
 
 function setRelations(){
     try {
+        User.hasOne(Contact_info, {
+            foreignKey: 'user_id'
+        })
+        Contact_info.belongsTo(User)
+
         Collection.hasMany(Catalogue)
         Catalogue.belongsTo(Collection)
-        
+
+        User.hasMany(Comment)
+        Comment.belongsTo(User)
+
+        Catalogue.hasMany(Comment)
+        Comment.belongsTo(Catalogue)
+
+        Catalogue.belongsToMany(Platform, { through: Platform_catalogue})
+        Platform.belongsToMany(Catalogue, { through: Platform_catalogue })
+
+        Catalogue.belongsToMany(User, { through: User_catalogue })
+        User.belongsToMany(Catalogue, { through: User_catalogue }) 
         
     } catch (error) {
         console.log(error)
