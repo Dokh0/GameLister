b4ab04d04dd683235f59f976151e68ea57a375ea
TheshiaR <cuentaparaclassroomtheshia@gmail.com>
2023-10-24 22:40:30 +0100

    platform functions

--- api/routes/platform.router.js
+++ api/routes/platform.router.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 
-const { getAllPlatforms, getOnePlatform, createPlatform, updatePlatform, deletePlatform, addGamePlatform } = require('../controllers/platform.controller')
+const { getAllPlatforms, getOnePlatform, getplatformCatalogue, createPlatform, updatePlatform, deletePlatform, addGamePlatform } = require('../controllers/platform.controller')
 const { checkAuth, checkAdmin }= require('../middleware/index')
 
 router.get('/', getAllPlatforms)
 router.get('/:id', getOnePlatform)
+router.get('/:id/catalogue', getplatformCatalogue)
 router.post('/', checkAuth, checkAdmin, createPlatform)
 router.post('/catalogue', checkAuth, checkAdmin, addGamePlatform)
 router.put('/:id', checkAuth, checkAdmin, updatePlatform)
