3931d317e6618743d776b8679cab80e7cd069021
TheshiaR <cuentaparaclassroomtheshia@gmail.com>
2023-10-24 10:00:13 +0100

    cambios

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -10,7 +10,7 @@ const User = sequelize.define(
             type: DataTypes.STRING, allowNull: false,
           },
         email: {
-            type: DataTypes.STRING, allowNull: false,
+            type: DataTypes.STRING, allowNull: false, unique: true
           },
         password: {
             type: DataTypes.STRING, allowNull: false,
